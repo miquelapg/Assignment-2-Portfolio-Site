49c05febcd77a84b63c26fdeb9f790588721f39e
miquelapg <miquela.gonzales@ontariotechu.net>
2024-11-03 21:24:00 -0500

    Creating routes, installing bootstrap, fontawesome, and jquery
    created 2 home, about, contact, and project routes
    changes titles for each route
    installed bootstrap and fontawesome
    modified index.ejs html code to use bootstrap and jquery responsive designs
    modified app.js to go through node_modules items

--- Assignment2Site/routes/index.js
+++ Assignment2Site/routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+/* GET Projects page. */
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
