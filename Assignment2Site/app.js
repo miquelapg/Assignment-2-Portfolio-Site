1c8a73d22329f27289e34c1240a0ee516b218888
miquelapg <miquela.gonzales@ontariotechu.net>
2024-11-04 01:05:00 -0500

    Creating Nav Bar and Partials folder
    created navigation bar, footer, and jumbotron
    added icons to navigation bar
    created main_nav, header, footer, and bottom_nav files in Partials folder
    error handling

--- Assignment2Site/app.js
+++ Assignment2Site/app.js
@@ -7,6 +7,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+const { title } = require('process');
 
 let app = express();
 
@@ -37,7 +38,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',
+  {
+    title: "Error"
+  }
+  );
 });
 
 module.exports = app;
